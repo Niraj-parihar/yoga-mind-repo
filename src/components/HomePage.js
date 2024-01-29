09f62d80104ba25bdd986f37bd032126ec8108f5
Niraj <nirajp.tt.19@nitj.ac.in>
2024-01-29 23:40:09 +0530

    created product page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,52 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 // components/HomePage.js
 import React from "react";
+import { Link } from "react-router-dom";
+import "./HomePage.css";
+import rightCornerImage from "../assets/Screenshot 2024-01-29 195159.png";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Yoga for Mind - Home Page</h1>
+      <div class="container">
+        <h1>Yoga for Mind</h1>
+        <div class="line-2">
+          <h3>
+            Watch Our Lead Yoga Thrapist. <u>Ms Uma Subramaniam </u>explain
+            <br />
+            Yoga for Mind
+          </h3>
+        </div>
+
+        <img
+          src={rightCornerImage}
+          alt="Right Corner Image"
+          className="right-corner-image"
+        />
+        <div class="line-3">
+          <h3>is this for you?</h3>
+        </div>
+
+        <div>
+          <h3>Hard to feel calm or sleep because of stress</h3>
+          <button>Book trial @99</button>
+        </div>
+        <div>
+          <h3>Hard to feel calm or sleep because of stress</h3>
+          <button>Book trial @99</button>
+        </div>
+        <div>
+          <h3>You have been advised by doctors to avoid physical activity</h3>
+          <Link
+            to="https://github.com/Niraj-parihar/yoga-mind-repo"
+            onClick={() => {
+              alert("Redirecting to github");
+            }}
+          >
+            i have a question
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,10 +1,75 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 // components/ProductPage.js
-import React from 'react';
+import React from "react";
+import "./ProductPage.css";
+import leftImage from "../assets/Screenshot 2024-01-29 215052.png";
+import { IoMdTime } from "react-icons/io";
+import { FcComments } from "react-icons/fc";
 
 const ProductPage = () => {
   return (
     <div>
-      <h1>Yoga for Mind - Product Page</h1>
+      <div class="container">
+        <div>
+          <h1 class="yoga-heading">Yoga for Mind</h1>
+        </div>
+
+        <div class="left-image">
+          <img src={leftImage} alt="left Image" className="left-image" />
+        </div>
+
+        <div class="yoga-description">
+          <div>
+            <h1>What is Yoga for Mind</h1>
+          </div>
+          <div>
+            <h3>
+              The Yoga for Minf involves physical movement and relies on
+              technique such as pranayama and meditation, Suited for women who
+              cannot perfrom more physical forms of yoga and are looking for
+              relief from concerns such as stress, anxiety, low mood etc
+            </h3>
+          </div>
+        </div>
+
+        <div class="package-description">
+          <div class="package-heading">
+            <h1>Choose your package</h1>
+          </div>
+          <div class="subscription-details">
+            <div class="price-row">
+              <h2>6 Months</h2>
+              <h4>916 / month</h4>
+
+              <button>BUY Now</button>
+            </div>
+            <div class="price-row">
+              <h2>3 Months</h2>
+              <h4>999 / month</h4>
+
+              <button>BUY Now</button>
+            </div>
+            <div class="price-row">
+              <h2>1 Months</h2>
+              <h4>1199 / month</h4>
+              <button>BUY Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="course-benefit">
+          <div className="header">
+            <h1>What you'll get</h1>
+          </div>
+          <div>
+            <FcComments />
+            {
+              " A chat group on thne Elda app wahere you can connect with your traniner and the rest of the elda Yoga Community."
+            }
+            <IoMdTime />
+            {" Ability to reschudule your class if you miss your session "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
