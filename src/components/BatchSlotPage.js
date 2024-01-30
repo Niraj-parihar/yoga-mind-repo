e99cf1cfa4304b6d62fc665ad2cda7b522694f4e
Niraj <nirajp.tt.19@nitj.ac.in>
2024-01-30 12:41:02 +0530

    new updates

--- src/components/BatchSlotPage.js
+++ src/components/BatchSlotPage.js
@@ -1,10 +1,14 @@
 // components/BatchSlotPage.js
-import React from 'react';
+import React from "react";
+import "./BatchSlotPage.css";
+import image from "../assets/Screenshot 2024-01-29 215052.png";
 
 const BatchSlotPage = () => {
   return (
     <div>
-      <h1>Yoga for Mind - Yoga Batch & Slot</h1>
+      <div className="front-image" style={{width:"500px", height:"250px", backgroundColor:"gray"}}>
+        <img src={image} alt="frontImage" />
+      </div>
     </div>
   );
 };
